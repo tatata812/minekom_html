23234bec0dc9080cd0851335acbbcae526610dce
tatata812 <75353660+tatata812@users.noreply.github.com>
2024-01-06 23:21:27 +0900

    PC完成

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,6 @@ $(function () {
   });
 
 
-
-
   // ヘッダー隠れる動き
 
   let startPos = 0;
@@ -84,56 +82,6 @@ $(function () {
     ],
   });
 
-
-
-
-  // 客室詳細
-  $slide = $('.room-main-js');
-  $navigation = $('.room-main__bottom-img');
-
-  $slide.slick({ //slickスライダー作成
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    fade: true,
-    dots: true,
-  });
-  $navigation.each(function (index) { //サムネイルに連番付与属性
-    $(this).attr('data-number', index);
-  });
-  $navigation.eq(0).addClass('current'); //1枚をオーバーレイ
-
-  $navigation.on('click', function () { //サムネイルクリック時イベント
-    var number = $(this).attr('data-number');
-    $slide.slick('slickGoTo', number, true);
-    $(this).siblings().removeClass('current');
-    $(this).addClass('current');
-  });
-
-
-  function checkBreakPoint() {
-    w = $(window).width();
-    if (w <= 600) {
-      // スマホ向け（767px以下のとき）
-      $('.top-plan-js').not('.slick-initialized').slick({
-        //スライドさせる
-        autoplay: false,
-        dots: true,
-        arrows: false,
-        slidesToShow: 1,
-      });
-    } else {
-      // PC向け
-      $('.top-plan-js.slick-initialized').slick('unslick');
-    }
-  }
-  // ウインドウがリサイズする度にチェック
-  $(window).resize(function () {
-    checkBreakPoint();
-  });
-  // 初回チェック
-  checkBreakPoint();
 
   //フェードイン
   $(window).scroll(function () {
