04a913b40909dbb2c676f03577d0d9333bf1dfa6
tatata812 <75353660+tatata812@users.noreply.github.com>
2024-01-13 02:00:33 +0900

    施工実績

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,6 +83,24 @@ $(function () {
   });
 
 
+  // 施工事例詳細
+  $(".js-sub-img img").on("click", function () {
+    // メイン画像に切り替えるimgのsrc取得
+    img = $(this).attr("src");
+    // currentクラス付け替え(枠線などを変えたい時に)
+    $(".js-sub-img").removeClass("current");
+    $(this).parent().addClass("current");
+    // fadeOutできたらsrc変更してfadeIn
+    $(".js-main-img img").fadeOut(500, function () {
+      $(".js-main-img img")
+        .attr("src", img)
+        .on("load", function () {
+          $(this).fadeIn(300);
+        });
+    });
+  });
+
+
   //フェードイン
   $(window).scroll(function () {
     const windowHeight = $(window).height(); //ウィンドウの高さ
